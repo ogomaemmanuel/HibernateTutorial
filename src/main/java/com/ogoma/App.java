085ecf277a3c53ac38cce3224a6d12029dd733c1
Emmanuel <ogoma.emmanuel@gmail.com>
2019-10-10 13:43:41 +0300

    first commit

---
+++ src/main/java/com/ogoma/App.java
@@ -0,0 +1,13 @@
+package com.ogoma;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

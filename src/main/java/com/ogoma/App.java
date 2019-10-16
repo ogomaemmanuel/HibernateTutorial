ae67331b5cc568c9351f8378fd8aa7c2cc42d7da
Emmanuel <ogoma.emmanuel@gmail.com>
2019-10-16 10:23:46 +0300

    added JPA module

--- src/main/java/com/ogoma/App.java
+++ src/main/java/com/ogoma/App.java
@@ -2,17 +2,23 @@ package com.ogoma;
 
 import com.ogoma.entities.Role;
 import com.ogoma.entities.User;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+
 import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashSet;
+
 
 /**
  * Hello world!
  */
 public class App {
+    private static Logger logger= LoggerFactory.getLogger(App.class);
     public static void main(String[] args) {
         //When not using hibernate.properties file use new Configuration().configure("hibernate.cfg.xml") ;
         Configuration configuration = new Configuration();
@@ -37,12 +43,11 @@ public class App {
         try {
             session.save(role);
             transaction.commit();
+            logger.debug("User details saved");
         } catch (Exception e) {
             transaction.rollback();
         } finally {
 
         }
-
-        System.out.println("Hello World!");
     }
 }

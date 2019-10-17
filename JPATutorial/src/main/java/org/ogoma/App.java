5acf329a3836ca28f969bd7121868ee00a218125
Emmanuel <ogoma.emmanuel@gmail.com>
2019-10-17 12:11:26 +0300

    added Jpa entity manager, Entity transaction and Enitymanagenager factory in main method of the JPa app

--- JPATutorial/src/main/java/org/ogoma/App.java
+++ JPATutorial/src/main/java/org/ogoma/App.java
@@ -1,13 +1,24 @@
 package org.ogoma;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpa-tuts");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction= entityManager.getTransaction();
+        entityTransaction.begin();
+
+        entityTransaction.commit();
+
+        System.out.println("Hello World!");
     }
+
 }

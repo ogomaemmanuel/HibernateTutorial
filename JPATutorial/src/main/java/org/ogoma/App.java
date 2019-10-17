fb61983fb7897ac722cccdce6a56436ffe627162
Emmanuel <ogoma.emmanuel@gmail.com>
2019-10-17 13:47:53 +0300

    added persisting user in Jpa tutorial module

--- JPATutorial/src/main/java/org/ogoma/App.java
+++ JPATutorial/src/main/java/org/ogoma/App.java
@@ -1,9 +1,12 @@
 package org.ogoma;
 
+import org.ogoma.entities.User;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.Date;
 
 /**
  * Hello world!
@@ -13,12 +16,16 @@ public class App {
 
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpa-tuts");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
-        EntityTransaction entityTransaction= entityManager.getTransaction();
+//        Session session=entityManager.unwrap(Session.class);
+
+        EntityTransaction entityTransaction = entityManager.getTransaction();
         entityTransaction.begin();
-
+        User user = new User();
+        user.setFirstName("Emmanuel");
+        user.setLastName("Ogoma");
+        user.setDob(new Date());
+        entityManager.persist(user);
         entityTransaction.commit();
-
-        System.out.println("Hello World!");
     }
 
 }

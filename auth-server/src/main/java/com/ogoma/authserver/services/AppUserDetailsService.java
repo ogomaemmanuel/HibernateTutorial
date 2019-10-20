940510b232d59db0f9a2c58dba7475dcf22a95fe
Emmanuel <ogoma.emmanuel@gmail.com>
2019-10-20 19:47:08 +0300

    Minor changes

--- auth-server/src/main/java/com/ogoma/authserver/services/AppUserDetailsService.java
+++ auth-server/src/main/java/com/ogoma/authserver/services/AppUserDetailsService.java
@@ -9,17 +9,20 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+
 @Component
 public class AppUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-
         User user= userRepository.findByEmail(s);
         if (user==null){
             throw new UsernameNotFoundException("User does not exists");
         }
-        return new org.springframework.security.core.userdetails.User();
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),user.getPassword(),new ArrayList<>()
+        );
     }
 }
